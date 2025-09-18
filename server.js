a8a18265b3b5a355dfc97f3123c63f7930e82883
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-18 18:05:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
 origin: [
-      "http://localhost:5173",      // dev
+      "http://localhost:5173", "http://192.168.1.101:5173",   // dev
       "https://chatly-app-pearl.vercel.app" // vercel frontend
     ],
     methods: ["GET", "POST"],
